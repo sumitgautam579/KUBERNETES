2e285ca906d12e4ec194724b1374352fe93480c1
SUMIT <zazubee88@gmail.com>
2025-06-26 23:07:09 +0530

    KIND_Install

---
+++ KIND_CLUSTER/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# for downloading the kind cluster and installing on EC2 instance
+
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.29.0/kind-linux-amd64
+chmod +x ./kind
+sudo cp ./kind /usr/local/bin/kind
+
+
+VERSION="v1.33.0"
+URL="https://dl.k8s.io/release/${VERSION}/bin/linux/amd64/kubectl"
+INSTALL_DIR="/usr/local/bin"
+
+# Download and install kubectl
+curl -LO "$URL"
+chmod +x kubectl
+sudo mv kubectl $INSTALL_DIR/
+kubectl version --client
+
+# Clean up
+rm -f kubectl
+rm -rf kind
+
+echo "kind & kubectl installation complete."
